a28f72943e69da89d7d41a2b88816ef40572f74d
BuildTools <lenny.angst@easyid.ch>
2020-10-13 08:21:59 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+include "php/config.php";
+header("Access-Control-Allow-Origin: *");
+
+session_start();
+
+if (isset($_GET['code'])) {
+    $_SESSION['AUTH_CODE'] = $_GET['code'];
+    $_SESSION['STATE'] = $_GET['state'];
+    $_SESSION['authorized'] = true;
+    echo "Loginvorgang...";
+
+    $url = "https://api.authentication.husqvarnagroup.dev/v1/oauth2/token";
+    $params = array(
+        "grant_type" => "authorization_code",
+        "client_id" => getAppKey(),
+        "client_secret" => getAppSecret(),
+        "code" => $_SESSION['AUTH_CODE'],
+        "redirect_uri" => "https://automower.test/",
+        "state" => $_SESSION["STATE"]
+    );
+
+    $headers = array(
+            "content-type: application/x-www-form-urlencoded"
+    );
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+
+    $response = curl_exec($curl);
+    $code = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
+    curl_close($curl);
+
+    if ($code == 200) {
+        $response = json_decode($response, true);
+
+        $_SESSION['ACCESS_TOKEN'] = $response['access_token'];
+        $_SESSION['REFRESH_TOKEN'] = $response['refresh_token'];
+
+        echo $_SESSION['ACCESS_TOKEN'];
+
+        header("Location: /dashboard");
+    } else {
+        echo "<br>Es ist ein Fehler aufgetreten: " . $code . "<br>Response:<br><pre><code>" . $response . "</code></pre>";
+    }
+}
+
+if (!$_SESSION['authorized']) {
+    header("Location: https://api.authentication.husqvarnagroup.dev/v1/oauth2/authorize?client_id=376ed738-769f-4f29-a8d5-6c7f96720fc3&redirect_uri=https://automower.test/");
+} else {
+    header("Location: /dashboard");
+}
+
+?>
+
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Automower | Login</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</head>
+<body>
+
+
+
+</body>
+</html>
---
+++ php/getStatus.php
@@ -0,0 +1,102 @@
+<?php
+
+include "config.php";
+
+session_start();
+
+if (!isset($_SESSION['ACCESS_TOKEN'])) {
+    echo "401";
+    exit();
+}
+
+$url = "https://api.amc.husqvarna.dev/v1/mowers/{$_POST['mowerID']}";
+
+$headers = array(
+    "Authorization-Provider: husqvarna",
+    "Authorization: Bearer {$_SESSION['ACCESS_TOKEN']}",
+    "X-Api-Key:" . getAppKey()
+);
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+
+$response = curl_exec($curl);
+$code = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
+curl_close($curl);
+
+if ($code == 200) {
+
+    $json = json_decode($response, true);
+    $activity = $json['data']['attributes']['mower']['activity'];
+    $state = $json['data']['attributes']['mower']['state'];
+    $battery = $json['data']['attributes']['battery']['batteryPercent'];
+    $battery_icon = "battery-empty";
+
+    $activity_string = getActivityStrings()[$activity];
+    $state_string = getStateStrings()[$state];
+    $color = "bg-danger";
+    $nextStart = "";
+
+    switch ($activity) {
+        case "UNKNOWN":
+        case "NOT_APPLICABLE":
+            $color = "bg-secondary";
+            break;
+        case "MOWING":
+            $color = "bg-success";
+            break;
+        case "CHARGING":
+        case "PARKED_IN_CS":
+            if ($json['data']['attributes']['planner']['nextStartTimestamp'] != 0) {
+                $unix = ($json['data']['attributes']['planner']['nextStartTimestamp'] / 1000) - 7200;
+                $date = date("j.m.Y \u\m H:i", $unix);
+                $weekday = getWeekdayStrings()[date("N", $unix)];
+                $nextStart = "<br>Nächster Start: $weekday, $date";
+            } else {
+                if ($json['data']['attributes']['planner']['override']['action'] == "NO_SOURCE") {
+                    $nextStart = "<br>Wartet bis auf Weiteres";
+                }
+            }
+        case "LEAVING":
+        case "GOING_HOME":
+            $color = "bg-primary";
+            break;
+        case "STOPPED_IN_GARDEN":
+            $color = "bg-danger";
+            break;
+    }
+
+    if ($battery < 95) {
+        if ($battery < 70) {
+            if ($battery < 45) {
+                if ($battery < 20) {
+                    $battery_icon = "battery-empty";
+                } else {
+                    $battery_icon = "battery-quarter";
+                }
+            } else {
+                $battery_icon = "battery-half";
+            }
+        } else {
+            $battery_icon = "battery-three-quarters";
+        }
+    } else {
+        $battery_icon = "battery-full";
+    }
+
+    echo '<div class="card ' . $color . ' text-white mb-4">
+                                    <div class="card-body">Aktivität: ' . $activity_string . '<br>
+                                    Status: ' . $state_string . '
+                                    ' . $nextStart . '<br>
+                                    <div class="float-right"><i class="fas fa-' . $battery_icon . '"></i> ' . $battery . '%</div></div>
+                                </div>';
+
+} else {
+    http_response_code($code);
+    echo $response;
+    exit();
+}
