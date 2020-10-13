08a8190ff52e310b50a7bad2e0d1b8d448e6d1c3
BuildTools <lenny.angst@easyid.ch>
2020-10-13 10:11:14 +0200

    Add error message Add success message Add button loading animation

--- dashboard/js/start.js
+++ dashboard/js/start.js
@@ -5,12 +5,14 @@ $("input[name=modal-start-type]").on("change", function() {
         if ($("#modal-start-time-input").val() == "") {
             $("#modal-start-submit").attr("disabled", "disabled");
         } else {
-            $("#modal-start-submit").removeAttr("disabled");
+            if (!Start.loading)
+                $("#modal-start-submit").removeAttr("disabled");
         }
 
     } else {
         $("#modal-start-time").collapse("hide");
-        $("#modal-start-submit").removeAttr("disabled");
+        if (!Start.loading)
+            $("#modal-start-submit").removeAttr("disabled");
     }
 });
 
@@ -18,6 +20,32 @@ $("#modal-start-time-input").on("keyup", function () {
     if ($("#modal-start-time-input").val() == "") {
         $("#modal-start-submit").attr("disabled", "disabled");
     } else {
-        $("#modal-start-submit").removeAttr("disabled");
+        if (!Start.loading)
+            $("#modal-start-submit").removeAttr("disabled");
     }
-})
+});
+
+$("#modal-start-submit").on("click", function () {
+    Start.startLoadingAnimation();
+});
+
+class Start {
+
+    static loading = false;
+
+    static startLoadingAnimation() {
+        this.loading = true;
+        $("#modal-start-close").attr("disabled", "disabled");
+        $("#modal-start-submit").attr("disabled", "disabled");
+        $("#modal-start-submit").html("<span class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span>\n" +
+            "  Wird ausgeführt...");
+        $("#modal-start-closex").attr("disabled", "disabled");
+    }
+
+    static stopLoadingAnimation() {
+        this.loading = false;
+        $("#modal-start-close").removeAttr("disabled");
+        $("#modal-start-submit").removeAttr("disabled");
+        $("#modal-start-submit").html("Starten");
+    }
+}
